392192f59b2d44b298bcad3e99ceb63600183c08
Diego Maia <diegocmsantos@gmail.com>
2020-11-27 14:41:15 +0000

    Put and delete

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+
+    private $rules = [
+        'name' => 'required|max:255',
+        'is_active' => 'boolean'
+    ];
     
     public function index()
     {
@@ -17,10 +22,7 @@ class CategoryController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required|max:255',
-            'is_active' => 'boolean'
-        ]);
+        $this->validate($request, $this->rules);
         return Category::create($request->all());
     }
 
@@ -31,11 +33,14 @@ class CategoryController extends Controller
 
     public function update(Request $request, Category $category)
     {
-        //
+        $this->validate($request, $this->rules);
+        $category->update($request->all());
+        return $category;
     }
 
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return response()->noContent();
     }
 }
